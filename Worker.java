a2dc9206211149956c1cc5df7d96473427ab2c7a
Sam <124637623+Sammm03@users.noreply.github.com>
2025-01-04 13:41:53 +0000

    Update Worker.java

--- Worker.java
+++ Worker.java
@@ -1,27 +1,31 @@
-package service;
+package controller;
 
 import model.Customer;
 import model.Parcel;
-import util.Log;
+import model.QueueofCustomers;
+import model.ParcelMap;
 
 public class Worker {
-    private service.QueueofCustomers queue;
-    private service.ParcelMap parcelMap;
+    private QueueofCustomers queue;
+    private ParcelMap parcelMap;
 
-    public Worker(service.QueueofCustomers queue, service.ParcelMap parcelMap) {
+    public Worker(QueueofCustomers queue, ParcelMap parcelMap) {
         this.queue = queue;
         this.parcelMap = parcelMap;
     }
 
     public void processNextCustomer() {
         Customer customer = queue.removeCustomer();
-        Parcel parcel = parcelMap.getParcelById(customer.getSequenceNumber() + ""); // Example logic
+        if (customer != null) {
+            Parcel parcel = parcelMap.getParcelById(String.valueOf(customer.getSequenceNumber()));
+            if (parcel != null) {
+                float fee = parcel.calculateFee();
+                parcel.setStatus("collected");
 
-        float fee = parcel.calculateFee();
-        parcel.setStatus("collected");
-
-        Log.getInstance().logEvent("Processed customer: " + customer.getName() +
-                ", Parcel ID: " + parcel.getParcelId() +
-                ", Fee: " + fee);
+                Log.getInstance().logEvent("Processed customer: " + customer.getName() +
+                        ", Parcel ID: " + parcel.getParcelId() +
+                        ", Fee: " + fee);
+            }
+        }
     }
 }
