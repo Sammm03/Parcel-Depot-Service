69349f57189c9331dc8512476b40722f5000711b
Sam <124637623+Sammm03@users.noreply.github.com>
2025-01-04 13:39:18 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package view;
+
+import controller.Manager;
+import controller.Worker;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Manager manager = new Manager();
+        Worker worker = new Worker(manager.getQueue(), manager.getParcelMap());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        SwingUtilities.invokeLater(() -> {
+            MainFrame mainFrame = new MainFrame(manager, worker);
+            mainFrame.setVisible(true);
+        });
     }
 }
