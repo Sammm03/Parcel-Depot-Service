528b607eb84c78482367fde15e421f396efe76ca
Sam <124637623+Sammm03@users.noreply.github.com>
2025-01-05 13:10:06 +0000

    Update QueueofCustomers.java

--- QueueofCustomers.java
+++ QueueofCustomers.java
@@ -4,21 +4,29 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class QueueofCustomers {
-    private Queue<Customer> customers = new LinkedList<>();
+    private Queue<Customer> customers; // Non-static variable
+
+    public QueueofCustomers() {
+        customers = new LinkedList<>();
+    }
 
     public void addCustomer(Customer customer) {
-        customers.add(customer);
+        customers.add(customer); // Accessing the instance variable
     }
 
     public Customer removeCustomer() {
-        return customers.poll();
+        return customers.poll(); // Removes and returns the head of the queue
     }
 
-    public int getQueueSize() {
-        return customers.size();
+    public Customer peekCustomer() {
+        return customers.peek(); // Returns the head of the queue without removing
     }
 
     public Queue<Customer> getCustomers() {
-        return customers;
+        return customers; // Returns the queue
+    }
+
+    public boolean isEmpty() {
+        return customers.isEmpty();
     }
 }
