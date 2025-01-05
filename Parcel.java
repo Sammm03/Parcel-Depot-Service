716f93a405c4aff2d76f34d7879e98313a68efdf
Sam <124637623+Sammm03@users.noreply.github.com>
2025-01-05 13:09:32 +0000

    Update Parcel.java

--- Parcel.java
+++ Parcel.java
@@ -2,32 +2,72 @@ package model;
 
 public class Parcel {
     private String parcelId;
-    private float weight;
     private int daysInDepot;
+    private double weight;
+    private double length;
+    private double width;
+    private double height;
     private String status;
 
-    public Parcel(String parcelId, float weight, int daysInDepot, String status) {
+    public Parcel(String parcelId, int daysInDepot, double weight, double length, double width, double height) {
         this.parcelId = parcelId;
-        this.weight = weight;
         this.daysInDepot = daysInDepot;
-        this.status = status;
+        this.weight = weight;
+        this.length = length;
+        this.width = width;
+        this.height = height;
+        this.status = "waiting";
     }
 
-    public float calculateFee() {
-        float feePerKg = 10.0f;
-        float dailyStorageFee = 2.0f;
-        return (weight * feePerKg) + (daysInDepot * dailyStorageFee);
-    }
-
+    // Getters
     public String getParcelId() {
         return parcelId;
+    }
+
+    public int getDaysInDepot() {
+        return daysInDepot;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
     }
 
     public String getStatus() {
         return status;
     }
 
+    // Setters
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    // New method to get dimensions as a formatted string
+    public String getDimensions() {
+        return length + " x " + width + " x " + height;
+    }
+
+    @Override
+    public String toString() {
+        return "Parcel{" +
+                "parcelId='" + parcelId + '\'' +
+                ", daysInDepot=" + daysInDepot +
+                ", weight=" + weight +
+                ", length=" + length +
+                ", width=" + width +
+                ", height=" + height +
+                ", status='" + status + '\'' +
+                '}';
     }
 }
