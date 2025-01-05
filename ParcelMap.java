8f85e95768b7327a6dd4f2de6654f52c43b55627
Sam <124637623+Sammm03@users.noreply.github.com>
2025-01-05 13:09:49 +0000

    Update ParcelMap.java

--- ParcelMap.java
+++ ParcelMap.java
@@ -4,20 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ParcelMap {
-    private Map<String, Parcel> parcels = new HashMap<>();
+    private Map<String, Parcel> parcels;
 
+    public ParcelMap() {
+        parcels = new HashMap<>();
+    }
+
+    // Add a parcel to the map
     public void addParcel(Parcel parcel) {
-        parcels.put(parcel.getParcelId(), parcel);
+        parcels.put(parcel.getParcelId(), parcel); // Parcel ID as the key
     }
 
+    // Get a parcel by its ID
     public Parcel getParcelById(String parcelId) {
-        return parcels.get(parcelId);
+        return parcels.get(parcelId); // Retrieve the Parcel object
     }
 
-    public void removeParcel(String parcelId) {
-        parcels.remove(parcelId);
-    }
-
+    // Get all parcels
     public Map<String, Parcel> getParcels() {
         return parcels;
     }
